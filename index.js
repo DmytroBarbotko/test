9d573175aaddeca15949192da05173bdc9a292e8
Dmytro Barbotko <barbotko.developer@gmail.com>
2023-12-01 20:26:02 +0100

    Object creation

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-console.log("What movie have you watched?");
-
-const numberOfFilms = answer;
+const numberOfFilms = +prompt("How many movies have you already watched?", "");
 
 const personalMovieDB = {
-    count:
-    movies: 
-    actors:
-    genres:
-    privat:
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
 };
 
+const lastFilms = prompt("What was the last film you watched ?", ""),
+  assessment = prompt("How would you rate this?", ""),
+  lastFilmsSecond = prompt("What was the last film you watched ?", ""),
+  assessmentSecond = prompt("How would you rate this?", "");
 
+personalMovieDB.movies[lastFilms] = assessment;
+personalMovieDB.movies[lastFilmsSecond] = assessmentSecond;
+
+console.log(personalMovieDB);
